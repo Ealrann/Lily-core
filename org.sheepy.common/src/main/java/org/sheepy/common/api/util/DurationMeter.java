0cf2bd7f4c5e41d1a4090f488aa98edda620e1f8
Aurelien Mora <ealrann@gmail.com>
2018-11-13 17:37:40 +0800

    Custom units for DurationMeter

--- org.sheepy.common/src/main/java/org/sheepy/common/api/util/DurationMeter.java
+++ org.sheepy.common/src/main/java/org/sheepy/common/api/util/DurationMeter.java
@@ -1,47 +1,53 @@
 package org.sheepy.common.api.util;
 
+import java.util.concurrent.TimeUnit;
+
 public class DurationMeter
 {
 	private final int tickBeforeReport;
 	private final String name;
-	
+	private final TimeUnit reportUnit;
+
 	private int tick = 0;
 	private long duration = 0;
-	
 	private long timeStart = 0;
 
-	public DurationMeter(String name, int tickBeforeReport)
+	public DurationMeter(String name, int tickBeforeReport, TimeUnit reportUnit)
 	{
 		this.name = name;
 		this.tickBeforeReport = tickBeforeReport;
+		this.reportUnit = reportUnit;
 	}
-	
+
 	public void startRecord()
 	{
-		timeStart = System.currentTimeMillis();
+		timeStart = System.nanoTime();
 	}
-	
+
 	public void endRecord()
 	{
-		duration += System.currentTimeMillis() - timeStart;
-		tick ++;
-		
-		if(tick >= tickBeforeReport)
+		duration += System.nanoTime() - timeStart;
+		tick++;
+
+		if (tick >= tickBeforeReport)
 		{
 			printDuration();
 			reset();
 		}
 	}
-	
+
 	public void reset()
 	{
 		duration = 0;
 		tick = 0;
 	}
-	
+
 	private void printDuration()
 	{
-		float averageDuration = duration / tick;
-		System.out.println(String.format("%s: :%d ms", name, (int) averageDuration));
+		long averageDuration = duration / tick;
+		int convertedDuration = (int) reportUnit.convert(averageDuration, TimeUnit.NANOSECONDS);
+
+		System.out.println(String.format("%s: %d %s", name, convertedDuration,
+				reportUnit.toChronoUnit().name()));
 	}
 }
