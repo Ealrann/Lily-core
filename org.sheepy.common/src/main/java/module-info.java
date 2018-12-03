1edded68aa7fa7e3377806631eafd6cfb87c9b39
Aurélien Mora <ealrann@gmail.com>
2018-12-03 17:36:49 +0800

    Use EMF standalone (For full compliance with java 9)

--- org.sheepy.common/src/main/java/module-info.java
+++ org.sheepy.common/src/main/java/module-info.java
@@ -8,8 +8,6 @@ module org.sheepy.common
 	requires transitive org.eclipse.emf.common;
 	requires transitive org.eclipse.emf.ecore;
 
-	requires com.google.common;
-
 	exports org.sheepy.common.api.adapter;
 	exports org.sheepy.common.api.adapter.impl;
 	exports org.sheepy.common.api.types;
