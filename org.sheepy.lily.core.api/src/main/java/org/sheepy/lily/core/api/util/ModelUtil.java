d5a9a0e3d3bdb780bbad4de4aa5fb6643960aa75
Aurélien Mora <ealrann@gmail.com>
2019-04-10 01:10:57 +0800

    Add some util method (gatherChildEObjects, gatherChildren, )

--- org.sheepy.lily.core.api/src/main/java/org/sheepy/lily/core/api/util/ModelUtil.java
+++ org.sheepy.lily.core.api/src/main/java/org/sheepy/lily/core/api/util/ModelUtil.java
@@ -1,6 +1,9 @@
 package org.sheepy.lily.core.api.util;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Deque;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
@@ -20,31 +23,67 @@ public class ModelUtil
 		return (Application) eObject;
 	}
 
-	public static void gatherChildEObjects(EObject eo, Collection<EObject> gatherIn)
+	@SuppressWarnings("unchecked")
+	public static <T extends EObject> List<T> gatherAllChildren(EObject root, Class<T> type)
 	{
-		var containments = eo.eClass().getEAllContainments();
+		final List<T> res = new ArrayList<>();
+		final Deque<EObject> course = new ArrayDeque<>();
+
+		course.add(root);
+
+		while (course.isEmpty() == false)
+		{
+			final var currentEObject = course.pop();
+
+			if (type.isInstance(currentEObject))
+			{
+				res.add((T) currentEObject);
+			}
+
+			gatherChildren(currentEObject, course);
+		}
+
+		return res;
+	}
+
+	public static void gatherChildren(EObject eo, Collection<EObject> gatherIn)
+	{
+		gatherChildren(eo, EObject.class, gatherIn);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T extends EObject> void gatherChildren(	EObject eo,
+															Class<T> type,
+															Collection<T> gatherIn)
+	{
+		final var containments = eo.eClass().getEAllContainments();
 		for (int i = 0; i < containments.size(); i++)
 		{
-			var eReference = containments.get(i);
-			if (eReference.isMany())
+			final var eReference = containments.get(i);
+
+			if (type == null
+					|| type == EObject.class
+					|| eReference.getEReferenceType().getInstanceClass().isAssignableFrom(type))
 			{
-				@SuppressWarnings("unchecked")
-				var values = (List<EObject>) eo.eGet(eReference);
-				for (int j = 0; j < values.size(); j++)
+				if (eReference.isMany())
 				{
-					EObject value = values.get(j);
-					if (value != null)
+					final var values = (List<EObject>) eo.eGet(eReference);
+					for (int j = 0; j < values.size(); j++)
 					{
-						gatherIn.add(eo);
+						final EObject value = values.get(j);
+						if (value != null)
+						{
+							gatherIn.add((T) value);
+						}
 					}
 				}
-			}
-			else
-			{
-				var value = (EObject) eo.eGet(eReference);
-				if (value != null)
+				else
 				{
-					gatherIn.add(eo);
+					final var value = (EObject) eo.eGet(eReference);
+					if (value != null)
+					{
+						gatherIn.add((T) value);
+					}
 				}
 			}
 		}
