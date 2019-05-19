f3d71c390e63c5efe57b51adda00b20e153e2e4a
Aurélien Mora <ealrann@gmail.com>
2019-05-19 16:54:52 +0200

    Update line counter

--- org.sheepy.lily.core.resources/count_line.sh
+++ org.sheepy.lily.core.resources/count_line.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-find -type f \( -name *.java -o -name *.xcore \) ! -name package-info.java ! -path "*lily.lib*" ! -path *src-gen* | xargs wc -l
-#find -type f \( -name *.java -o -name *.xcore \) ! -name package-info.java ! -path *src-gen* | xargs wc -l
+find -type f \( -name *.java -o -name *.xcore \) ! -name package-info.java ! -path "*emf*" ! -path "*generated*" | xargs wc -l
