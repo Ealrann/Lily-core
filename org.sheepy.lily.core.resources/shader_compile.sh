016a5327c19c3389654a1403b55e88643d2aecd7
Aurélien Mora <ealrann@gmail.com>
2019-11-14 12:21:54 +0800

    Use glslc to compile shaders

--- org.sheepy.lily.core.resources/shader_compile.sh
+++ org.sheepy.lily.core.resources/shader_compile.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-optimize='true'
 
 for i in `find -regex '.*\.\(frag\|comp\|vert\)' ! -path "*/bin/*" ! -path "*/build/*" | grep "/src/main/shader" 2>/dev/null`
 do
 	target="${i/shader/resources}.spv"
 	rm $target 2>/dev/null
-	if [ $optimize == 'true' ]
-	then
-		dir=`dirname $target`
-		name=`basename $target`
-		glslangValidator -V $i -o /tmp/$name
-		spirv-opt -O /tmp/$name -o /tmp/$name
-		spirv-remap --strip all --dce all -i /tmp/$name -o $dir
-		rm /tmp/$name
-	else
-		glslangValidator -V $i -o $target
-	fi
+
+	dir=`dirname $target`
+	name=`basename $target`
+	glslc -O -c $i -o $target
+	echo "$target"
 done
