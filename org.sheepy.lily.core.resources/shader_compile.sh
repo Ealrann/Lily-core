792c7d701cb210de055c4b20db966cfe96bbbdd6
Aurélien Mora <ealrann@gmail.com>
2019-11-15 20:06:28 +0800

    Add status message to compute shader script

--- org.sheepy.lily.core.resources/shader_compile.sh
+++ org.sheepy.lily.core.resources/shader_compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-
+count=0
+error_count=0
 for i in `find -regex '.*\.\(frag\|comp\|vert\)' ! -path "*/bin/*" ! -path "*/build/*" | grep "/src/main/shader" 2>/dev/null`
 do
 	target="${i/shader/resources}.spv"
@@ -8,6 +9,18 @@ do
 
 	dir=`dirname $target`
 	name=`basename $target`
-	glslc -O -c $i -o $target
+	res=$(glslc -O --target-env="vulkan1.0" -c $i -o $target)
+	if [ $? -ne 0 ]; then
+		((error_count++))
+	fi
 	echo "$target"
+	((count++))
 done
+
+echo ""
+echo "Compilation finished"
+echo "$count file(s) compiled successfully"
+if [[ $error_count -ne 0 ]]; then
+	echo "$error_count file(s) failed to compile"
+fi
+echo ""
