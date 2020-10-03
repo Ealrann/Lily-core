8d91db9963e347f70918f9afcf24e87d63c88090
Aurélien Mora <ealrann@gmail.com>
2020-10-03 16:53:53 +0200

    IFileImageAdapter

---
+++ org.sheepy.lily.core.api/src/main/java/org/sheepy/lily/core/api/resource/IFileImageAdapter.java
@@ -0,0 +1,9 @@
+package org.sheepy.lily.core.api.resource;
+
+import org.joml.Vector2ic;
+import org.sheepy.lily.core.api.extender.IExtender;
+
+public interface IFileImageAdapter extends IExtender
+{
+	Vector2ic size();
+}
